1a5e3df6cdd6317a2a6861674c444309e028c17a
8Oldr <8Oldr@protonmail.com>
2023-09-26 21:12:09 +0300

    Well, very basic functionality is completed. The game doesn't end properly yet though

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <signal.h>
 #include <stdio.h>
 
 typedef enum GameScreen { LOGO, TITLE, GAMEPLAY, ENDING } GameScreen;
@@ -22,6 +23,52 @@ enum CellContents{
 };
 
 int GameBoard[9];
+const int GameTextSize = 80;
+const int LetterOffset = 20;
+const int EndTextOffset = 40;
+
+typedef enum GameEnd {
+    NOT_ENDED,
+    X_WIN,
+    O_WIN,
+    DRAW,
+    UNKNOWN,
+} GameEnd;
+
+enum GameEnd LineVictory(enum CellContents i, enum CellContents j, enum CellContents k) {
+    if (i == EMPTY || j == EMPTY || k == EMPTY) {
+        return NOT_ENDED;
+    } else if (i == X && j == X && k == X) {
+        return X_WIN;
+    } else if (i == O && j == O && k == O) {
+        return O_WIN;
+    }
+    return NOT_ENDED;
+}
+
+enum GameEnd CheckGameEnded() {
+    enum GameEnd GameStates[] = {
+        LineVictory(GameBoard[0], GameBoard[3], GameBoard[6]), // row0
+        LineVictory(GameBoard[1], GameBoard[4], GameBoard[7]), // row1
+        LineVictory(GameBoard[2], GameBoard[5], GameBoard[8]), // row2
+        LineVictory(GameBoard[0], GameBoard[1], GameBoard[2]), // column0
+        LineVictory(GameBoard[3], GameBoard[4], GameBoard[5]), // column1
+        LineVictory(GameBoard[6], GameBoard[7], GameBoard[8]), // column2
+        LineVictory(GameBoard[0], GameBoard[4], GameBoard[8]), // diagonal_ltr
+        LineVictory(GameBoard[2], GameBoard[4], GameBoard[8]), // diagonal_rtl
+    };
+
+    for (int i=0; i<8; i++) {
+        if (GameStates[i] == UNKNOWN) {
+            raise(420);
+            return UNKNOWN;
+        } else if (GameStates[i] != NOT_ENDED) {
+            return GameStates[i];
+        }
+    }
+    return NOT_ENDED;
+}
+
 
 enum WhichPlayer {
     PLAYER_0,
@@ -29,6 +76,7 @@ enum WhichPlayer {
 };
 
 enum WhichPlayer whichPlayer = PLAYER_0;
+enum GameEnd currentGameState = NOT_ENDED;
 
 int main()
 {
@@ -37,7 +85,7 @@ int main()
     const int cellWidth = screenWidth / 3;
     const int cellHeight = screenHeight / 3;
 
-    InitWindow(screenWidth, screenHeight, "PROJECT: BLOCKS GAME");
+    InitWindow(screenWidth, screenHeight, "TIC TAC TOE");
 
 
     GameScreen screen = LOGO;       // Current game screen state
@@ -78,13 +126,44 @@ int main()
             } break;
             case GAMEPLAY:
             {
+                if (!gamePaused && (currentGameState != DRAW || currentGameState != X_WIN || currentGameState != O_WIN)) {
+                    int playerInteraction = 0;
+                    if (IsKeyPressed(KEY_ZERO) && GameBoard[0] == EMPTY) {
+                        GameBoard[0] = whichPlayer + 1; // +1 because of the enums
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_ONE) && GameBoard[1] == EMPTY) {
+                        GameBoard[1] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_TWO) && GameBoard[2] == EMPTY) {
+                        GameBoard[2] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_THREE) && GameBoard[3] == EMPTY) {
+                        GameBoard[3] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_FOUR) && GameBoard[4] == EMPTY) {
+                        GameBoard[4] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_FIVE) && GameBoard[5] == EMPTY) {
+                        GameBoard[5] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_SIX) && GameBoard[6] == EMPTY) {
+                        GameBoard[6] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_SEVEN) && GameBoard[7] == EMPTY) {
+                        GameBoard[7] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    } else if (IsKeyPressed(KEY_EIGHT) && GameBoard[8] == EMPTY) {
+                        GameBoard[8] = whichPlayer + 1;
+                        playerInteraction = 1;
+                    }
 
-                if (!gamePaused)
-                    if (IsKeyPressed(KEY_ZERO)) {
-                        GameBoard[0] = X;
+                    if (playerInteraction == 1) {
+                        whichPlayer = !whichPlayer;
+                        currentGameState = CheckGameEnded();
                     }
 
                 if (IsKeyPressed(KEY_ENTER)) screen = ENDING;
+                }
 
             } break;
             case ENDING:
@@ -142,33 +221,22 @@ int main()
                                 14,
                                 BLACK
                         );
+                        char cellOccupant[1];
 
                         if (GameBoard[i] == X) {
-                            DrawText(
-                                "X",
-                                position.x + cellWidth / 2,
-                                position.y + cellHeight / 2,
-                                20,
-                                BLACK
-                            );
+                            cellOccupant[0] = 'X';
                         } else if (GameBoard[i] == O) {
-                            DrawText(
-                                "O",
-                                position.x + cellWidth / 2,
-                                position.y + cellHeight / 2,
-                                20,
-                                BLACK
-                            );
+                            cellOccupant[0] = 'O';
                         } else {
-                            DrawText(
-                                "EMPTY",
-                                position.x + cellWidth / 2,
-                                position.y + cellHeight / 2,
-                                20,
-                                BLACK
-                            );
-
+                            cellOccupant[0] = '-';
                         }
+                        DrawText(
+                            cellOccupant,
+                            position.x + cellWidth / 2 - LetterOffset,
+                            position.y + cellHeight / 2 - LetterOffset,
+                            GameTextSize,
+                            BLACK
+                        );
                     }
                     const char* whichPlayerString = "Player %s";
                     if (whichPlayer == PLAYER_0) {
@@ -177,13 +245,39 @@ int main()
                         whichPlayerString = TextFormat(whichPlayerString, "1");
                     }
                     DrawText(
-                        "Player 0",
+                        whichPlayerString,
                         screenWidth / 2 - 80,
                         20,
                         40,
                         BLACK
                     );
 
+
+                    if (currentGameState == DRAW) {
+                        DrawText(
+                            "DRAW",
+                            screenWidth / 2 - EndTextOffset,
+                            screenHeight / 2 - EndTextOffset,
+                            100,
+                            RED
+                        );
+                    } else if (currentGameState == X_WIN) {
+                        DrawText(
+                            "X WINS",
+                            screenWidth / 2 - EndTextOffset,
+                            screenHeight / 2 - EndTextOffset,
+                            100,
+                            RED
+                        );
+                    } else if (currentGameState == O_WIN) {
+                        DrawText(
+                            "O WINS",
+                            screenWidth / 2 - EndTextOffset,
+                            screenHeight / 2 - EndTextOffset,
+                            100,
+                            RED
+                        );
+                    }
                 } break;
                 case ENDING:
                 {
